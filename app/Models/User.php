562ac917132f27973df324bed6323af9c41817d2
vlad <kurganov@ittest-team.ru>
2023-10-01 16:16:47 +0400

    init

---
+++ app/Models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    use HasApiTokens;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
